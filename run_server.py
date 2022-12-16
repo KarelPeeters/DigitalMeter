1297299d4c79e17fc392d6cc75ef6309d5c8d02d
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-12-16 17:57:27 +0100

    Generalize run_server.

--- run_server.py
+++ run_server.py
@@ -6,22 +6,30 @@ from functools import partial
 from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 from http.server import test as http_server_test
 
-mimetypes.add_type("application/javascript", ".js")
-mimetypes.add_type("text/html", ".html")
+
+def run_server(directory):
+    mimetypes.add_type("application/javascript", ".js")
+    mimetypes.add_type("text/html", ".html")
+
+    class DualStackServer(ThreadingHTTPServer):
+        def server_bind(self):
+            # suppress exception when protocol is IPv4
+            with contextlib.suppress(Exception):
+                self.socket.setsockopt(
+                    socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+            return super().server_bind()
+
+    http_server_test(
+        HandlerClass=partial(SimpleHTTPRequestHandler, directory=directory),
+        ServerClass=DualStackServer,
+        port=8000,
+        bind=None,
+    )
 
 
-class DualStackServer(ThreadingHTTPServer):
-    def server_bind(self):
-        # suppress exception when protocol is IPv4
-        with contextlib.suppress(Exception):
-            self.socket.setsockopt(
-                socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
-        return super().server_bind()
+def main():
+    run_server(os.getcwd())
 
 
-http_server_test(
-    HandlerClass=partial(SimpleHTTPRequestHandler, directory=os.getcwd()),
-    ServerClass=DualStackServer,
-    port=8000,
-    bind=None,
-)
+if __name__ == '__main__':
+    main()
