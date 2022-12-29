6a4ee73f2c2180ba490dd6ff5771a6f74a1ae1b7
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-12-29 16:23:57 +0100

    Rewrite fetch_series query to enable using an index scan instead of a full scan, which is too slow.

--- server/socket_server.py
+++ server/socket_server.py
@@ -102,22 +102,32 @@ class MultiSeries:
 # TODO send nan for missing values instead of nothing, so JS doesn't just interpolate
 
 def bucket_bounds(window_size: int, bucket_size: int, timestamp: int) -> (int, int):
-    oldest = timestamp // bucket_size * bucket_size - window_size - bucket_size
-    newest = timestamp // bucket_size * bucket_size - bucket_size
+    """
+    Compute the bounds `min` (inclusive), `max` (exclusive) of all finished buckets,
+    assuming the sample with `timestamp` is the latest one in the database.
+    """
+    oldest = (timestamp + 1) // bucket_size * bucket_size - window_size
+    newest = (timestamp + 1) // bucket_size * bucket_size
     return oldest, newest
 
 
 def fetch_series_items(database: Connection, bucket_size: int, oldest: int, newest: int):
+    """
+    Fetch the buckets between `oldest` (inclusive) and `newest` (exclusive)`.
+    """
     start = time.perf_counter()
-    result = database.execute("WITH const as (SELECT ? as bucket_size, ? as oldest, ? as newest) " \
-                                "SELECT timestamp / bucket_size * bucket_size, " \
-                                "AVG(instant_power_1)," \
-                                "AVG(instant_power_2)," \
-                                "AVG(instant_power_3)" \
-                                "FROM meter_samples, const " \
-                                "WHERE oldest < timestamp / bucket_size * bucket_size AND timestamp / bucket_size * bucket_size <= newest " \
-                                "GROUP BY timestamp / bucket_size " \
-                                "ORDER BY timestamp ", (bucket_size, oldest, newest)).fetchall()
+    result = database.execute(
+        "WITH const as (SELECT ? as bucket_size, ? as oldest, ? as newest) "
+        "SELECT timestamp / bucket_size * bucket_size, "
+        "AVG(instant_power_1),"
+        "AVG(instant_power_2),"
+        "AVG(instant_power_3)"
+        "FROM meter_samples, const "
+        "WHERE oldest <= timestamp AND timestamp < newest "
+        "GROUP BY timestamp / bucket_size "
+        "ORDER BY timestamp ",
+        (bucket_size, oldest, newest)
+    ).fetchall()
     print(f"Fetch took {time.perf_counter() - start}")
     return result
 
@@ -136,25 +146,26 @@ class Tracker:
 
     def process_message(self, database: Connection, msg: Message):
         prev_timestamp = self.last_timestamp
-        self.last_timestamp = msg.timestamp
+        curr_timestamp = msg.timestamp
+        self.last_timestamp = curr_timestamp
 
         delta_multi_series = MultiSeries({})
 
         for key in self.multi_series.map:
             series = self.multi_series.map[key]
-            curr_oldest, curr_newest = bucket_bounds(series.window_size, series.bucket_size, self.last_timestamp)
+            curr_oldest, curr_newest = bucket_bounds(series.window_size, series.bucket_size, curr_timestamp)
 
             if prev_timestamp is None:
                 # fetch the entire series
                 print(f"Fetching entire series for '{key}'")
                 new_items = fetch_series_items(database, series.bucket_size, curr_oldest, curr_newest)
             else:
-                # only fetch new buckets
-                print(f"Fetching new buckets for '{key}'")
+                # only fetch new buckets if any
                 _, prev_newest = bucket_bounds(series.window_size, series.bucket_size, prev_timestamp)
                 if curr_newest == prev_newest:
                     continue
                 else:
+                    print(f"Fetching new buckets for '{key}'")
                     new_items = fetch_series_items(database, series.bucket_size, prev_newest, curr_newest)
 
             # put into cached series
