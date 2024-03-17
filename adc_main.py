dc084d71ce058f542fa6ec2613f5d8a12ed18572
Karel Peeters <karel.peeters.leuven@gmail.com>
2024-03-17 14:18:06 +0000

    Make adc readout time configurable

--- adc_main.py
+++ adc_main.py
@@ -9,19 +9,20 @@ pin_data = gpiozero.DigitalInputDevice("GPIO21")
 pin_reset_n.value = 1
 pin_reset_n.value = 1
 
+T=0.1
 
 def reset():
     pin_reset_n.value = 0
-    time.sleep(0.1)
+    time.sleep(T)
     pin_reset_n.value = 1
-    time.sleep(0.1)
+    time.sleep(T)
 
 
 def next():
     pin_next_n.value = 0
-    time.sleep(0.1)
+    time.sleep(T)
     pin_next_n.value = 1
-    time.sleep(0.1)
+    time.sleep(T)
 
 
 def readout():
